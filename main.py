01d0a493049d5451a83c8e3b57509997cdb6145c
Copas Lopez <46980925+Copas-Lopez@users.noreply.github.com>
2019-02-12 17:48:33 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,30 +1,46 @@
 from urllib.request import urlopen
-from datetime import datetime
 from tqdm import tqdm
-from time import sleep
+import re
+from datetime import datetime
+import sys
+import statistics
+from operator import itemgetter
+import heapq
+import collections
+import os
+import time
 
+
+def least_common_values(array, to_find=None):
+    counter = collections.Counter(array)
+    if to_find is None:
+        return sorted(counter.items(), key=itemgetter(1), reverse=False)
+    return heapq.nsmallest(to_find, counter.items(), key=itemgetter(1))
+
+def cls():
+    os.system('cls' if os.name=='nt' else 'clear')
+
+
+#Apache Webserver format:
+#%h %l %u %t \"%r\" %>s %b \"%{Referer}i\" \"%{User-Agent}i\
+regex = '(.*?) - (.*) \[(.*?)\] (.*?) (\d+) (.)'
+
+print('GETTING FILE...')
 #Get log from link
 url = "https://s3.amazonaws.com/tcmg476/http_access_log"
 response = urlopen(url)
 log = str(response.read(), "utf-8")
-
-#Create and write contents to text file
-text_file = open("Output.txt", "w")
-text_file.write(log[0:71])
-text_file.close()
+print("COMPLETE")
+time.sleep(1)
+cls()
 
 #Split log into lines for easier parsing
-test = log.splitlines()
-lines = test
+lines = log.splitlines()
 
-
-#Setting base variables for all our counting
-days = []
-months = []
-years = []
-total_unknowns = 0
-redirects = 0
-files = []
+total_requests = len(lines)
+total_malformed = 0
+total_client_errors = 0
+total_redirects = 0
 
 Jan = []
 Feb = []
@@ -39,67 +55,73 @@ Oct = []
 Nov = []
 Dec = []
 file_months = [Jan, Feb, Mar, Apr, May, Jun, Jul, Aug, Sep, Oct, Nov, Dec]
-
 month_names = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
 
-#Main loop to go through each line
+file_names = []
+
+day_requests = []
+week_requests = []
+month_requests = []
+
+print('PARSING DATA...')
+
+last_day = 0
+week_total = 0
+requests_day = 0
+
+#Main loop
 for i in tqdm(range(len(lines))):
+    match = re.match(regex,lines[i])
+    if match:
+        #Split the line apart for each section
+        curr_line = list(match.groups())
 
-    #==== REQUIREMENT 1 ====#
-    total_request = len(lines)
-    curr_line = lines[i]
-    #print(curr_line)
+        #Date parsing
+        date = curr_line[2].strip('[').split(':')[0]
+        formated_date = datetime.strptime(date, '%d/%b/%Y')
+        file_months[formated_date.month - 1].append(lines[i])
+        if formated_date.weekday() == last_day:
+            requests_day = requests_day + 1
+            week_total = week_total + 1
+        else:
+            day_requests.append(requests_day)
+            requests_day = 1
+            last_day = formated_date.weekday()
+            if formated_date.weekday() == 0:
+                week_requests.append(week_total)
+                week_total = 0
+        #Status Code
+        code = list(curr_line[4])
+        if code[0] is '4':
+            total_client_errors = total_client_errors + 1
+        elif code[0] is '3':
+            total_redirects = total_redirects + 1
 
-    #Split the line
-    split = curr_line.split(" ")
+        #Files
+        request = curr_line[3].split(' ')
+        file_names.append(request[1])
 
-    #Special case checking format of line
-    if len(split) < 8:
-        continue
-
-    next = list(split[2])
-    #print(next)
-    if next[0] is "[":
-        date_time = split[2].strip("[").split(":")
-        date = date_time[0]
-        date = datetime.strptime(date, "%d/%b/%Y")
-
-        #Add line into month matrix
-        file_months[date.month - 1].append(curr_line)
     else:
-        date_time = split[3].strip("[").split(":")
-        date = date_time[0]
-        date = datetime.strptime(date, "%d/%b/%Y")
-
-        #Add line into month matrix
-        file_months[date.month - 1].append(curr_line)
-
-    #Get Percentages
-
-    if len(split) < 9:
-        continue
-    else:
-        #Split codes
-        code = split[8].split()
-        #Check for 404
-        if code[0] == "4":
-            total_unknowns = total_unknowns+1
-        elif code[0] == "3":
-            redirects = redirects+1
-
-            #Get files
-            files.append(split[6])
-
-    pass
-
-for i in range(len(file_months)):
+        #ALL malformed requests are ignored
+        total_malformed = total_malformed + 1
+cls()
+print('WRITING MONTHLY FILES...')
+for i in tqdm(range(len(file_months))):
     string_name = "./month_text/" + month_names[i] + ".txt"
     month_file = open(string_name, "a")
-    for d in range(len(file_months[i])):
+    for d in tqdm(range(len(file_months[i]))):
         month_file.write(file_months[i][d] + "\n")
     month_file.close
-##Write to monthly files
-#string_name = "./month_text/" + file_months[date.month - 1] + ".txt"
-#month_file = open(string_name, "a")
-#month_file.write(curr_line + "\n")
-#month_file.close()
+    month_requests.append(len(file_months[i]))
+cls()
+c = collections.Counter(file_names).most_common()[-1]
+print('==== Statistics ====')
+print('Total Requests:: ' + str(total_requests))
+print('Total Malformed Requests:: ' + str(total_malformed) + ' - ' + str(round(((total_malformed / total_requests)*100),2)) + '%')
+print('Total Client Errors:: ' + str(total_client_errors) + ' - ' + str(round(((total_client_errors / total_requests)*100),2)) + '%')
+print('Total Redirects:: ' + str(total_redirects) + ' - ' + str(round(((total_redirects / total_requests)*100),2)) + '%')
+print('Average Requests Made Per Day:: ' + str(round(statistics.mean(day_requests))))
+print('Average Requests Made Per Week:: ' + str(round(statistics.mean(week_requests))))
+print('Average Requests Made Per Month:: ' + str(round(statistics.mean(month_requests))))
+print('Most Common File:: ' + statistics.mode(file_names))
+print('Least Common File:: ' + str(c[0]))
